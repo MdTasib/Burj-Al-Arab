bf2a2005e1ef93dae49ac8c964815a7265259fdd
MdTasib <ohidulalam442@gmail.com>
2021-05-11 13:12:48 +0600

    Book page

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { UserContext } from '../../App';
 
 const Book = () => {
     const { bedType } = useParams();
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     return (
         <div style={{ textAlign: 'center' }}>
-            <h1>Let's book a <b style={{ color: 'tomato' }}>{bedType} Room.</b></h1>
+            <h1>Hello, {loggedInUser.name}! Let's book a <b style={{ color: 'tomato' }}>{bedType} Room.</b></h1>
             <strong>Want a <Link to="/home"><b>Different room?</b></Link> </strong>
         </div>
     );
